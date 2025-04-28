cdcee910b15f9f5177f3fbeaa8d9ef5a9e1352fb
Diana-MND1996 <dianamarcela.nova@unavarra.es>
2025-04-28 20:17:51 +0200

    Microsim_AlterTherapies.R

--- Microsim_AlterTherapies.R
+++ Microsim_AlterTherapies.R
@@ -3,14 +3,14 @@
 ############################################################################################
 # This code forms the basis for the microsimulation model of the article: 
 #
-# Diana Marcela Nova Dí­az, Sergio Aguilera Albesa, Eduardo Sánchez Iriso. 
-# Microsimulation modeling for health decision sciences using R: A tutorial. 
-# Cost-Effectiveness Analysis of Complementary and Alternative Therapies in Children with Cerebral Palsy
-#
+# Diana Marcela Nova Díaz, Sergio Aguilera Albesa, Eduardo Sánchez Iriso. 
+# Cost-Effectiveness of Complementary and Alternative Therapies for Children with Cerebral Palsy: 
+# Evidence from Real-World Data and Microsimulation Modelling
+
 # Please cite the article when using this code
 
 ############################################################################################
-################# Code of Appendix A #######################################################
+################# Code of Appendix A.1 #######################################################
 ############################################################################################
 #rm(list = ls())  # remove any variables in R's memory 
 
