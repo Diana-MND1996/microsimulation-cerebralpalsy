1f1ac3226983092e5c52b8b5d319dbcb220ae964
Diana-MND1996 <dianamarcela.nova@unavarra.es>
2025-04-30 21:15:06 +0200

    MICROSIM_PHE.R

--- MICROSIM_PHE.R
+++ MICROSIM_PHE.R
@@ -20,7 +20,7 @@ n.t   <- 30                         # 30-year time horizon
 v.n   <- c("GMFCS I-II", "GMFCS III", "GMFCS IV-V", "Dead")  # Health statuses: GMFCS I-II (Near-healthy: H), GMFCS III (Sick: S1), GMFCS IV-V (Sicker: S2), Dead (D)
 n.s   <- length(v.n)                # The number of health states
 v.M_1 <- rep("GMFCS I-II", n.i)     # All start in the healthy state (H: GMFCS I-II)
-d.c   <- d.e <- 0.03                # Descuento de costos y QALYs al 3%
+d.c   <- d.e <- 0.03                # Discount rate for costs and QALYs 3%
 v.Trt <- c("No Treatment", "Standard treatment", "Complementary therapies", "Alternative therapies")
 
 # Note: For coding efficiency, "Complementary therapies" and "Alternative therapies" are shorthand labels. 
